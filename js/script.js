e44847cbb372ab1f387364efc6d85302efef5e3e
Jose Pereda <joselpereda@hotmail.com>
2021-03-18 01:36:46 -0400

    added function to get DOM elements

--- js/script.js
+++ js/script.js
@@ -1,59 +1,89 @@
 // GLOBAL VARIABLES
-// var employeeId;
-// var employeeName;
-// var extension;
-// var email;
-// var department;
-// var submit;
+let employeeId;
+let employeeName;
+let extension;
+let email;
+let department;
+let form;
 
-const f = selector => document.querySelector(selector);
+window.addEventListener('load', () => {
+
+    //GET DOM ELEMENTS
+    const $ = (idValue) => {
+        return document.getElementById(idValue);
+    };
+
+    //GET USER INPUT VALUES
+    employeeId =            $("id").value;
+    employeeName =          $("name").value;
+    extension =             $("extension").value;
+    email =                 $("email").value;
+    department =            $("department").value;
+
+    //GET THE FORMS'S ID
+    form =                  window.document.getElementById("empForm");    
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        console.log('hello world - 1');
+        console.log('hello world - 2');
+        console.log('hello world - 3');
+    });
+    
+    console.log(employeeName);
+});
 
 
-// GET DOM ELEMENTS
-// const employeeId =            window.document.getElementById("id");
-// const employeeName =          window.document.getElementById("name");
-// const extension =             window.document.getElementById("ext");
-// const email =                 window.document.getElementById("email");
-// const department =            window.document.getElementById("department");
-// const submit =                window.document.getElementById("submit");
 
-const processEntries = () => {
-    const employeeId =              f("#id");
-    const employeeName =            f("#name");
-    const extension =               f("#ext");
-    const email =                   f("#email");
-    const department =              f("#department");
 
-   // window.console.log(`ID: ${employeeId}`);
-    window.console.log(employeeName);
+// const f = selector => document.querySelector(selector);
 
-};
 
-// submit.addEventListener("click", function () {
-//     window.console.log(`ID: ${employeeId}`);
+// // GET DOM ELEMENTS
+// // const employeeId =            window.document.getElementById("id");
+// // const employeeName =          window.document.getElementById("name");
+// // const extension =             window.document.getElementById("ext");
+// // const email =                 window.document.getElementById("email");
+// // const department =            window.document.getElementById("department");
+// // const submit =                window.document.getElementById("submit");
+
+// const processEntries = () => {
+//     const employeeId =              f("#id");
+//     const employeeName =            f("#name");
+//     const extension =               f("#ext");
+//     const email =                   f("#email");
+//     const department =              f("#department");
+
+//    // window.console.log(`ID: ${employeeId}`);
+//     window.console.log(employeeName);
+
+// };
+
+// // submit.addEventListener("click", function () {
+// //     window.console.log(`ID: ${employeeId}`);
+// // });
+
+// //window.console.log(`ID: ${employeeId}`);
+// // window.console.log("name");
+// // window.console.log("ext");
+// // window.console.log("email");
+// // window.console.log("department");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     f("#submit").addEventListener("click", processEntries);
 // });
 
-//window.console.log(`ID: ${employeeId}`);
-// window.console.log("name");
-// window.console.log("ext");
-// window.console.log("email");
-// window.console.log("department");
-
-document.addEventListener("DOMContentLoaded", () => {
-    f("#submit").addEventListener("click", processEntries);
-});
-
 	
-window.addEventListener('load', init);
-    function init() {
-        let btn = document.getElementById('email');
-        btn.addEventListener('click', function () {
-            //console.log(employeeId);		
-        });	
-    }
-/*--window.addEventListener('submit', )*/
+// window.addEventListener('load', init);
+//     function init() {
+//         let btn = document.getElementById('email');
+//         btn.addEventListener('click', function () {
+//             //console.log(employeeId);		
+//         });	
+//     }
+// /*--window.addEventListener('submit', )*/
 
-window.addEventListener('submit', function (e) {
-    e.preventDefault();
-});
+// window.addEventListener('submit', function (e) {
+//     e.preventDefault();
+// });
 
